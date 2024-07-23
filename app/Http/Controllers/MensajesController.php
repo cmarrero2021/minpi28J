825c8e64f4df2fe4d7669c976920e85e5b45ebbf
cmarrero2021 <marrero.c@gmail.com>
2024-07-23 10:17:52 -0400

    202407223_1016 Se integró el script de procesamiento de mensajes; listo el sistema minpi

--- app/Http/Controllers/MensajesController.php
+++ app/Http/Controllers/MensajesController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Mensajes;
+use App\Models\Electore;
+use Illuminate\Support\Facades\DB;
 
 class MensajesController extends Controller
 {
@@ -45,17 +47,52 @@ class MensajesController extends Controller
         ]);
     }
     public function process_sms(Request $request) {
-        $text = $request->query('text');
-        $sender = $request->query('sender');
-        $date = $request->query('date');
-        $time = $request->query('time');
-        \Log::info('SMS recibido', [
-            'text' => $text,
-            'sender' => $sender,
-            'date' => $date,
-            'time' => $time,
-        ]);
-        return response("SMS processed successfully.", 200)->header('Content-Type', 'text/plain');
+        $texto_msj = $request->query('text');
+        $ced = substr($texto_msj, 5);
+        $nac = substr($texto_msj, 4,1);
+        $cedu = preg_replace('/[.,\- ]/', '', $ced);
+        if ((ctype_digit($cedu) && strlen($cedu) >= 6 && strlen($cedu) <= 8) && ($nac === 'V' || $nac === 'E')) {
+            $cedula = (int)$cedu;
+            $fecha_msj = $request->query('date');
+            $anio = substr($fecha_msj, 0, 4);
+            $mes = str_pad(trim(substr($fecha_msj, 4, 2)),2,'0',STR_PAD_LEFT);
+            $dia = str_pad(trim(substr($fecha_msj, 6, 2)),2,'0',STR_PAD_LEFT);
+            $fecha = sprintf('%s-%s-%s', $anio, $mes, $dia);
+            $hora_msj = $request->query('time');
+            $hora_m = str_pad(trim(substr($hora_msj, 0, 2)),2,'0',STR_PAD_LEFT);
+            $minuto = str_pad(trim(substr($hora_msj, -4, 2)),2,'0',STR_PAD_LEFT);
+            $segundo = str_pad(trim(substr($hora_msj, -2, 2)),2,'0',STR_PAD_LEFT);
+            $hora = sprintf('%02d:%02d:%02d', $hora_m, $minuto, $segundo);
+            $telefono = $request->query('sender');   
+            $datos = [
+               'nacionalidad' => $nac,
+               'cedula' => $cedula,
+               'fecha' => $fecha,
+               'hora' => $hora,
+               'telefono' => $telefono
+            ];
+            $elector = Electore::where('cedula', $datos['cedula'])->first();
+            if ($elector) {
+                $elector->voto = true;
+                $elector->hora_voto = $datos['hora'];
+                $elector->save();
+            } else {
+                $respuesta = "La cédula $nac$cedula no está registrada en nuestra base de datos, revise e intente de nuevo";
+                return response($respuesta, 200)->header('Content-Type', 'text/plain');
+            }
+            $mensaje = Mensajes::where('cedula', $datos['cedula'])->first();
+            if ($mensaje) {
+                $respuesta = "La cédula $nac$cedula ya ha sido registrada previamente";
+                return response($respuesta, 200)->header('Content-Type', 'text/plain');
+            } else {
+                Mensajes::create($datos);
+                $respuesta = "La Cédula $nac$cedula ha sido registrada correctamente";
+                return response($respuesta, 200)->header('Content-Type', 'text/plain');
+            }            
+        } else {
+            $respuesta = "El formato del mensaje debe ser 28J N99999999 en número, sin espacios, puntos ni guiones y se debe dejar un sólo espacio entre 28J y el número de cédula para que pueda ser procesado; adicionalmente, la longitud de la cédula sólo puede tener entre 6 y 8 números. Corrija la CI ".$nac.$ced;
+            return response($respuesta, 200)->header('Content-Type', 'text/plain');
+        }
     }
 
 }
